a562d68f1514d663e89f32532daf01970fccd22d
AlexVipond <alex.r.vipond@gmail.com>
2020-07-07 09:57:32 -0600

    Adding nested dir support

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-import { readdirSync } from 'fs'
+import { readdirSync, lstatSync } from 'fs'
 
-export default function fromDirToRoutes ({ router, dir }) {
-  const routes = readdirSync(dir),
-        withoutExtensions = routes.map(page => page.replace(/\.\w+$/, '')),
-        withRouterFormatting = withoutExtensions.map(name => toFormattedRoute({ name, router }))
+const fileNameRegExp = /([\w\d-])\.\w+$/,
+      fileExtensionRegExp = /(\.)(\w+)$/,
+      dirNameRegExp = /([\w\d-])/,
+
+export default function fromDirToRoutes ({ router, pathToDir: { absolute, relativeFromRoutes } }) {
+  const paths = toPaths(absolute),
+        withMetadata = paths.map(path => ({
+          name: path.match(fileNameRegExp)[1],
+          extension: path.match(fileExtensionRegExp)[2],
+          path = {
+            relativeFromDir: path.replace(absolute, '').replace(fileNameRegExp, '').replace(fileExtensionRegExp, ''),
+            relativeFromRoutes,
+          }
+          
+        })),
+        withRouterFormatting = withMetadata.map(fileMetadata => toFormattedRoute({ fileMetadata, router })).join(',')
   
   return `export default [${withRouterFormatting}]`
 }
 
-function toFormattedRoute({ name, router }) {
-  return toFormattedRoutes[router](name)
+function toPaths (absolute) {
+  return readdirSync(absolute)
+    .filter(item => fileNameRegExp.test(item) || dirNameRegExp.test(item))
+    .reduce((files, item) => {
+      item = isFile({ absolute, item }) ? [`${absolute}/${item}`] : toPaths(`${absolute}/${item}`)
+      return [
+        ...files,
+        ...item
+      ]
+    }, [])
 }
 
-const toFormattedRoutes = {
-  vue: name => `{ path: '/${name}', name: '${name}', component: import('../tests/${name}.vue') }`
+function isFile ({ absolute, item }) {
+  return lstatSync(`${absolute}/${item}`).isFile()
+}
+
+function toFormattedRoute({ fileMetadata, router }) {
+  return toFormattedRouteByRouter[router](fileMetadata)
+}
+
+const toFormattedRouteByRouter = {
+  vue: ({ name, extension, path: { relativeFromDir, relativeFromRoutes }}) => `{ path: '/${relativeFromDir}/${name}', name: '${name}', component: import('${relativeFromRoutes}/${name}.${extension}') }`
 }
